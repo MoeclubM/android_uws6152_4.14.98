29ce4552f5767d801b56bb118c93cc9e2135c2cc
MoeclubM <qytc233@gmail.com>
2026-08-30 23:10:11 +0800

    fix(trace): set TRACE_INCLUDE_PATH to . matching CFLAGS -I$(src)

--- drivers/devfreq/sprd/sprd_dfs_trace.h
+++ drivers/devfreq/sprd/sprd_dfs_trace.h
@@ -71,7 +71,7 @@ TRACE_EVENT(sprd_dfs_poll,
 
 /* This part must be outside protection */
 #undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH drivers/devfreq/sprd
+#define TRACE_INCLUDE_PATH .
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_FILE sprd_dfs_trace
 #include <trace/define_trace.h>
